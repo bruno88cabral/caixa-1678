5d98f5509410fb3c799637238430eeb4ffe7259f
bruno cabral <bruno88cabral@gmail.com>
2026-05-13 19:25:18 -0300

    adicionando exemplo na aula 3

---
+++ aula-3/exemplo.js
@@ -0,0 +1,42 @@
+const contatos = [
+  {
+    nome: "Bruno",
+    telefone: "1199999999",
+    dataDeNascimento: "04/04/1988",
+    endereco: {
+      rua: "rua teste",
+      numero: "100",
+      bairro: "bairro teste",
+    },
+  },
+  {
+    nome: "Bruno",
+    telefone: "1199999999",
+    dataDeNascimento: "04/04/1988",
+    endereco: {
+      rua: "rua teste",
+      numero: "100",
+      bairro: "bairro teste",
+    },
+  },
+  {
+    nome: "Bruno",
+    telefone: "1199999999",
+    dataDeNascimento: "04/04/1988",
+    endereco: {
+      rua: "rua teste",
+      numero: "100",
+      bairro: "bairro teste",
+    },
+  },
+  {
+    nome: "Bruno",
+    telefone: "1199999999",
+    dataDeNascimento: "04/04/1988",
+    endereco: {
+      rua: "rua teste",
+      numero: "100",
+      bairro: "bairro teste",
+    },
+  },
+];
